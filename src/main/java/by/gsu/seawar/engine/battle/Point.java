540a863a8135a2a541aead5a5797bcaa63979bd9
Vitali Vikhliayeu <Vitali_Vikhliayeu@epam.com>
2017-10-09 12:41:53 +0300

    fix

--- src/main/java/by/gsu/seawar/engine/battle/Point.java
+++ src/main/java/by/gsu/seawar/engine/battle/Point.java
@@ -1,8 +1,11 @@
 package by.gsu.seawar.engine.battle;
 
+import by.gsu.seawar.enums.FieldStatus;
+
 public class Point {
     private int x;
     private int y;
+    private FieldStatus status;
 
     public Point(int x, int y) {
         super();
@@ -10,16 +13,24 @@ public class Point {
         this.y = y;
     }
 
-    public int getX() {
-        return x;
-    }
+	public FieldStatus getStatus() {
+		return status;
+	}
 
-    public void setX(int x) {
-        this.x = x;
+	public int getX() {
+        return x;
     }
 
     public int getY() {
         return y;
+    }
+
+    public void setStatus(FieldStatus status) {
+		this.status = status;
+	}
+
+    public void setX(int x) {
+        this.x = x;
     }
 
     public void setY(int y) {
